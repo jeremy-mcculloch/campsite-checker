f0c0816261e9a8fb6cbfe60f2d595e4ee3d1d58e
Jeremy McCulloch <jeremymcculloch@DN0a2257c8.SUNet>
2023-04-25 14:49:21 -0700

    added documentation

--- main_program.py
+++ main_program.py
@@ -4,10 +4,14 @@ from utils.camping_argparser import CampingArgumentParser
 import time
 
 def test_job():
-    main_permits([72192], CampingArgumentParser.TypeConverter.date("2023-05-05"), CampingArgumentParser.TypeConverter.date("2023-05-06"))
+    main_permits(
+        [72192], #when you search for permits the url should be https://www.recreation.gov/permits/[#####], replace 72192 with the number at the end 
+        CampingArgumentParser.TypeConverter.date("2023-05-05"), #First date that you are interested in permits for
+        CampingArgumentParser.TypeConverter.date("2023-05-06") # Last date that you are interested in permits for (make same as start date if only interested in one day)
+    )
 
 scheduler = BackgroundScheduler()
-job = scheduler.add_job(test_job, 'interval', minutes=10)
+job = scheduler.add_job(test_job, 'interval', minutes=10) #How frequently to query 
 scheduler.start()
 
 while True:
